8054cf4ea5eb7bcec9836ea0ae2bec903d595b89
Victor <victor.h.alves7@gmail.com>
2023-08-03 14:54:26 -0300

    second test (actually 7 tests, but yaknow) passed successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, reps) {
+    if (reps === 0) {return('')}
+    else if (reps < 0) {return('ERROR')}
+    else {
+        let wordHolder = word;
+        for (let i = 1; i < reps; i++) {
+        word += wordHolder}
+    return(word)}
 };
 
 // Do not edit below this line
