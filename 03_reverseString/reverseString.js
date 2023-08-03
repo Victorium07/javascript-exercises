1f4dcdf66198d89d8070407c1a2b99857f858793
Victor <victor.h.alves7@gmail.com>
2023-08-03 18:26:52 -0300

    third exercise, word reverse function is done and ready to be used

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let numberOfChar = word.length;
+    let reversedWord = '';
+    for (let i = numberOfChar ; i > -1; i--) {
+        let previousChar = word.slice(i, i+1);
+        reversedWord += previousChar;
+    }
+    return(reversedWord)
 };
 
 // Do not edit below this line
