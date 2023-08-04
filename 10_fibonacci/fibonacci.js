1b39a90bda835073a318bff4c23ca0408b2c6846
Victor <victor.h.alves7@gmail.com>
2023-08-04 18:37:01 -0300

    Donezoed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {return 'OOPS'}
+    index = Number(index);
+    let prevElement = 0;
+    let currElement = 1;
+    let adder = 0;
+    for (let i = 1; i < index; i++) {
+        prevElement = currElement;
+        currElement += adder;
+        adder = prevElement;
+    };
+    return(currElement);
 };
 
 // Do not edit below this line
