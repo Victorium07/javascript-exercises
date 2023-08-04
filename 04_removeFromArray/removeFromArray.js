3e727c9c762d642d74b79362b08ba45f71abfbad
Victor <victor.h.alves7@gmail.com>
2023-08-04 11:18:36 -0300

    Right, the flat vs flatMap thing really got me good. Thankfully google saved me on this one.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array) {
+    let arrayInt = array.flat();
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arrayInt.indexOf(arguments[i]);
+        if (index > -1) {
+            arrayInt.splice(index, 1);
+        }
+    }
+    return arrayInt;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
