fb94ea7d172b564fb1a68c12c68ccc163a332ea7
Victor <victor.h.alves7@gmail.com>
2023-08-04 11:33:00 -0300

    This one was easier than the previous one, not gonna lie

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let min = 0;
+    let max = 0;
+    let sum = 0;
+    if (firstNumber !== Number(firstNumber) || secondNumber !== Number(secondNumber)) {return 'ERROR'}
+    if (firstNumber <= secondNumber) {
+        min = firstNumber;
+        max = secondNumber;
+    } else {
+        min = secondNumber;
+        max = firstNumber;
+    }
+    if (min < 0) {return 'ERROR'};
+    for (min; min < max+1; min++) {
+        sum += min;
+    }
+    return sum;
 };
 
 // Do not edit below this line
