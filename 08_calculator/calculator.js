35bffe36c76398166e6562d31040664fca0e2e35
Victor <victor.h.alves7@gmail.com>
2023-08-04 15:21:12 -0300

    Lots of functions to make, but they were all simple individually. Cool.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+	for (let i = 0; i < args.length; i++) {
+    sum +=args[i]
+  };
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let difference = args[0] - args[1];
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let elements of array) {
+    sum +=elements;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+	for (let elements of array) {
+    product *=elements;
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(numerator, exponent) {
+  let power = 1;
+	for (let i = 0; i < exponent; i++) {
+    power *= numerator;
+  };
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+	if (number === 0) {return 1}
+  else {
+    for (number; number > 0; number--) {
+      factorial *= number;
+    };
+  };
+  return factorial;
 };
 
 // Do not edit below this line
