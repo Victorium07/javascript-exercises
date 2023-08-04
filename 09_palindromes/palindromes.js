f59fe8635881ab1441fb9abe2caa0f00053409ed
Victor <victor.h.alves7@gmail.com>
2023-08-04 18:14:51 -0300

    the actual function was fine, but I was having a hard time because I didn't close some brakets...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    sentence = sentence.toLowerCase().replaceAll('!', '').replaceAll(',', '').replaceAll(' ','').replaceAll('.', '');
+    let wordLength = sentence.length;
+    let reversedWord = '';
+    for (let i = wordLength; i > 0; i--) {
+        reversedWord += sentence.slice(i-1,i);
+    };
+    if (reversedWord === sentence) {return true}
+    else {return false};
 };
 
 // Do not edit below this line
