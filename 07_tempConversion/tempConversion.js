fbe5d0da166e16119390cdf8f5a2caf277072574
Victor <victor.h.alves7@gmail.com>
2023-08-04 12:01:28 -0300

    Cool one, I liked the rounding function more than the unit conversion one to be honest.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+function roundToFirstDecimal(number) {
+  let decimal = (number * 10) - (Math.floor(number) * 10);
+  if (decimal > 5) {return(Math.floor(number) + ((Math.floor(decimal)+1)/10));}
+  else {return(Math.floor(number) + (Math.floor(decimal)/10))}
+}
+
+const convertToCelsius = function(number) {
+  let tempCelsius = ((number - 32) * 5)/9;
+  tempCelsius = roundToFirstDecimal(tempCelsius);
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let tempFahrenheit = ((number * 9)/5) + 32;
+  tempFahrenheit = roundToFirstDecimal(tempFahrenheit);
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
